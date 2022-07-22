6c0687d09cae7d33e5d76364e690cb3e3584a333
cesar calle <makercode@gmail.com>
2022-07-21 22:56:07 -0500

    fixing architecture to pages and components and adding home routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SvgComponent } from './architecture/svg/svg.component';
-import { PathComponent } from './architecture/svg/components/path/path.component';
-import { TodoistComponent } from './architecture/todoist/todoist.component';
-import { TodoComponent } from './architecture/todoist/components/todo/todo.component'
+import { SvgComponent } from './components/svg/svg.component';
+import { PathComponent } from './components/svg/components/path/path.component';
+import { TodoistComponent } from './components/todoist/todoist.component';
+import { TodoComponent } from './components/todoist/components/todo/todo.component'
 
 import { DepartmentService } from './business/department/services/department.service';
 import { HomeComponent } from './pages/home/home.component';
@@ -26,6 +26,8 @@ import { HomeComponent } from './pages/home/home.component';
   providers: [
     DepartmentService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
