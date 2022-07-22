0bb77746b18e142596e6559677a94c8055e05079
cesar calle <makercode@gmail.com>
2022-07-22 13:02:15 -0500

    order app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Main components
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+
+// Secondary components
 import { AppComponent } from './app.component';
 import { SvgComponent } from './components/svg/svg.component';
 import { PathComponent } from './components/svg/components/path/path.component';
 import { TodoistComponent } from './components/todoist/todoist.component';
 import { TodoComponent } from './components/todoist/components/todo/todo.component'
 
+// Services
 import { DepartmentService } from './business/department/services/department.service';
-import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+
     SvgComponent,
     PathComponent,
     TodoistComponent,
-    TodoComponent,
-    HomeComponent
+    TodoComponent
   ],
   imports: [
     BrowserModule,
